34ec6e4d4e72e68c743f58bba68c101ac6637d67
Th3At0nic <islammdrahatul@gmail.com>
2023-05-14 11:13:53 +0600

    successfully connected the server with the client, shows user in or out with name

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
+const clientPort = "http://localhost:3000";
 
 const router = require("./router");
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
 
-io.on("connention", (socket) => {
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: clientPort,
+  },
+});
+
+io.on("connection", (socket) => {
   console.log("We have a new connection!!!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+  });
 
   socket.on("disconnect", () => {
     console.log("User had left!!!");
