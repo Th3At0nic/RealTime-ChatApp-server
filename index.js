907712b8fcebdc0208321330c8fb28062a6da772
Th3At0nic <islammdrahatul@gmail.com>
2023-05-14 22:42:44 +0600

    completed the server side locally, remaining deploy

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ io.on("connection", (socket) => {
     if (user) {
       io.to(user.room).emit("message", {
         user: "admin",
-        text: `${user.name} has left`,
+        text: `${user.name} has left the conversation`,
       });
     }
   });
